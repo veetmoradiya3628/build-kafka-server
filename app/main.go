62e51b173e845beb2eba1c3f8c25befb21a4956c
veet <veetmoradiya3628@gmail.com>
2026-08-10 16:32:08 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,24 +1,51 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
 )
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buff := make([]byte, 1024)
+	_, err := conn.Read(buff)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+
+	// 8 byte response
+	response := make([]byte, 8)
+	binary.BigEndian.PutUint32(response[0:4], 0)
+	binary.BigEndian.PutUint32(response[4:8], 7)
+
+	_, err = conn.Write(response)
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		return
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 }
