50bd4bf2cf0b9496d6fe8259fce2a36a17511ed0
veet <veetmoradiya3628@gmail.com>
2026-08-12 03:54:03 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -35,68 +35,70 @@ func parseRequest(buff []byte) (Request, error) {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	buff := make([]byte, 1024)
-	n, err := conn.Read(buff)
-	if err != nil {
-		fmt.Println("Error reading from connection: ", err.Error())
-		return
-	}
+	for {
+		buff := make([]byte, 1024)
+		n, err := conn.Read(buff)
+		if err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			return
+		}
 
-	req, err := parseRequest(buff[:n])
-	if err != nil {
-		fmt.Println("Error parsing request: ", err.Error())
-		return
-	}
+		req, err := parseRequest(buff[:n])
+		if err != nil {
+			fmt.Println("Error parsing request: ", err.Error())
+			return
+		}
 
-	fmt.Printf("Request received - API Key: %d, Version: %d, CorrelationID: %d\n", req.RequestAPIKey, req.RequestAPIVer, req.CorrelationID)
+		fmt.Printf("Request received - API Key: %d, Version: %d, CorrelationID: %d\n", req.RequestAPIKey, req.RequestAPIVer, req.CorrelationID)
 
-	apiVersion := binary.BigEndian.Uint16(buff[6:8])
-	fmt.Println("request received with API Version: ", apiVersion)
+		apiVersion := binary.BigEndian.Uint16(buff[6:8])
+		fmt.Println("request received with API Version: ", apiVersion)
 
-	var errorCode int16 = 0
-	// If it's an ApiVersions request (18) and the version is not between 0 and 4
-	if req.RequestAPIKey == 18 && (req.RequestAPIVer < 0 || req.RequestAPIVer > 4) {
-		errorCode = 35 // UNSUPPORTED_VERSION error code
-	}
+		var errorCode int16 = 0
+		// If it's an ApiVersions request (18) and the version is not between 0 and 4
+		if req.RequestAPIKey == 18 && (req.RequestAPIVer < 0 || req.RequestAPIVer > 4) {
+			errorCode = 35 // UNSUPPORTED_VERSION error code
+		}
 
-	// response
-	var body bytes.Buffer
+		// response
+		var body bytes.Buffer
 
-	// header
-	correlationID := binary.BigEndian.Uint32(buff[8:12])
-	binary.Write(&body, binary.BigEndian, correlationID)
+		// header
+		correlationID := binary.BigEndian.Uint32(buff[8:12])
+		binary.Write(&body, binary.BigEndian, correlationID)
 
-	// body
-	// error code 0
-	binary.Write(&body, binary.BigEndian, errorCode)
-	// api_keys array length (Compact Array format: Number of elements + 1)
-	binary.Write(&body, binary.BigEndian, int8(2))
+		// body
+		// error code 0
+		binary.Write(&body, binary.BigEndian, errorCode)
+		// api_keys array length (Compact Array format: Number of elements + 1)
+		binary.Write(&body, binary.BigEndian, int8(2))
 
-	// Array Element 1: API Key 18 (ApiVersions)
-	binary.Write(&body, binary.BigEndian, int16(18)) // api_key
-	binary.Write(&body, binary.BigEndian, int16(0))  // min_version
-	binary.Write(&body, binary.BigEndian, int16(4))  // max_version
-	binary.Write(&body, binary.BigEndian, int8(0))   // TAG_BUFFER for element
+		// Array Element 1: API Key 18 (ApiVersions)
+		binary.Write(&body, binary.BigEndian, int16(18)) // api_key
+		binary.Write(&body, binary.BigEndian, int16(0))  // min_version
+		binary.Write(&body, binary.BigEndian, int16(4))  // max_version
+		binary.Write(&body, binary.BigEndian, int8(0))   // TAG_BUFFER for element
 
-	// throttle_time_ms
-	binary.Write(&body, binary.BigEndian, int32(0))
+		// throttle_time_ms
+		binary.Write(&body, binary.BigEndian, int32(0))
 
-	// TAG_BUFFER for response body
-	binary.Write(&body, binary.BigEndian, int8(0))
+		// TAG_BUFFER for response body
+		binary.Write(&body, binary.BigEndian, int8(0))
 
-	// Prepend the message_size dynamically
-	// The message_size is the length of the Header + Body (excluding the 4 size bytes)
-	responseBytes := body.Bytes()
-	messageSize := uint32(len(responseBytes))
+		// Prepend the message_size dynamically
+		// The message_size is the length of the Header + Body (excluding the 4 size bytes)
+		responseBytes := body.Bytes()
+		messageSize := uint32(len(responseBytes))
 
-	var finalResponse bytes.Buffer
-	binary.Write(&finalResponse, binary.BigEndian, messageSize) // Write 4-byte size
-	finalResponse.Write(responseBytes)                          // Write the rest of the payload
+		var finalResponse bytes.Buffer
+		binary.Write(&finalResponse, binary.BigEndian, messageSize) // Write 4-byte size
+		finalResponse.Write(responseBytes)                          // Write the rest of the payload
 
-	// Send response
-	_, err = conn.Write(finalResponse.Bytes())
-	if err != nil {
-		fmt.Println("Error writing to connection:", err.Error())
+		// Send response
+		_, err = conn.Write(finalResponse.Bytes())
+		if err != nil {
+			fmt.Println("Error writing to connection:", err.Error())
+		}
 	}
 }
 
